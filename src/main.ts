c1964599c9e58c59c6bc3248edba93aa0b2bbfff
ckm960411 <a01098956117@gmail.com>
2022-10-02 18:43:51 +0900

    add global HttpExceptionFilter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/interceptors/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // class-validator 를 전역으로 적용
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(process.env.PORT, () => {
     console.log(`app is listening at ${process.env.PORT}`);
   });
