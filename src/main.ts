b81bde09c7386226ce9945bba9c0544cb28c62ec
ckm960411 <a01098956117@gmail.com>
2022-10-02 18:44:18 +0900

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe()); // class-validator 를 전역으로 적용
   app.useGlobalFilters(new HttpExceptionFilter());
 
+  app.enableCors({
+    origin: true, // true: 전체 허용 / 개발이 끝나면 어떤 특정 url 에서 접근이 가능할지 써주어야 함
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT, () => {
     console.log(`app is listening at ${process.env.PORT}`);
   });
