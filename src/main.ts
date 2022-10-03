380da8db79b19fa30a2b572afb0b5e31971b098e
ckm960411 <a01098956117@gmail.com>
2022-10-03 15:03:39 +0900

    add url global prefix

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
 
   const configService = app.get<ConfigService>(ConfigService);
 
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe()); // class-validator 를 전역으로 적용
   app.useGlobalFilters(new HttpExceptionFilter());
 
@@ -32,7 +33,7 @@ async function bootstrap() {
     .setVersion('1.0.0')
     .build();
   const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // Swagger API 에 엔드포인트를 지정해줌
+  SwaggerModule.setup('docs', app, document); // Swagger API 에 엔드포인트를 지정해줌
 
   const PORT = configService.get<string>('PORT');
 
